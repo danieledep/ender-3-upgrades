06f8ac08639fd2674c49ff9ce0a042b115acd4cb
dan.dep <dan.depietri@gmail.com>
2022-01-23 20:18:48 +0100

    Update shutdown.py

--- raspberry-remote-shutdown/shutdown.py
+++ raspberry-remote-shutdown/shutdown.py
@@ -3,7 +3,7 @@ import os
 import sys
 import paho.mqtt.client as mqtt
 import time
-broker="192.168.0.203"
+broker="192.168.1.121"
 
 #define callback
 def on_message(client, userdata, message):
@@ -19,14 +19,14 @@ def on_connect(client, userdata, flags,rc):
         client.bad_connection_flag=True
         client.connected_flag=False
 
-##MQTT
+## MQTT
 messages=[]
 sub_topic="octoprint/shutdown/#"
 client= mqtt.Client("Octoprint")
 ######
 client.on_message=on_message
 client.on_connect=on_connect
-client.username_pw_set(username="admin", password="admin")
+## client.username_pw_set(username="admin", password="admin")
 client.connected_flag=False
 client.connect(broker)#connect
 while True:
@@ -35,5 +35,5 @@ while True:
    if len(messages)>0:
       m=messages.pop(0)
       print("shutdown")
-      client.publish("prusa/mains/sonoff", payload=None, qos=0, retain=False)
+      client.publish("ender/mains", payload=None, qos=0, retain=False)
       os.system("sudo shutdown now")
