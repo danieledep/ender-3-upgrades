85bad7228d857227eb39481a57386fb1c094683c
dan.dep <dan.depietri@gmail.com>
2022-01-24 16:52:16 +0100

    Update install.sh

--- raspberry-remote-shutdown/install.sh
+++ raspberry-remote-shutdown/install.sh
@@ -8,8 +8,9 @@ sudo pip3 install esphome
 sudo apt install -y mosquitto mosquitto-clients
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 
-echo "installing service"
+echo "installing services"
 
+sudo systemctl enable nodered.service
 sudo cp shutdown.service /etc/systemd/system/shutdown.service
 sudo systemctl enable shutdown.service
 sudo systemctl start shutdown.service
